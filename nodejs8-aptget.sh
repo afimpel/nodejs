32ca58ca4f9a7157c9eba4d4ebf4913a0958040f
alvaro fimpel <afimpel@gmail.com>
2017-08-24 18:37:46 -0300

    Apt-get update automatico...

--- nodejs8-aptget.sh
+++ nodejs8-aptget.sh
@@ -1,5 +1,5 @@
+cd /root
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-sudo apt-get update
-apt-get install -y nodejs mongodb-server
+apt-get update
+apt-get install -y mongodb-server
+apt-get upgrade -y
