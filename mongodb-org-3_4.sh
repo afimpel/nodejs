9b71e2e2da397b410ee544fb513f8f71b5ceebc4
Alvaro Fimpel <afimpel@afimpel-x230>
2018-07-17 20:17:32 -0300

    Add existing file

--- mongodb-org-3_4.sh
+++ mongodb-org-3_4.sh
@@ -1,6 +1,16 @@
-cd /root
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" >> /etc/apt/sources.list.d/mongodb-org-3.4.list
-wget -qO- https://www.mongodb.org/static/pgp/server-3.4.asc?_ga=2.184220325.209513483.1503609550-378600733.1503609550 | apt-key add -
-apt-get update
-apt-get install -y nodejs mongodb-server
-apt-get upgrade -y
+#!/bin/sh
+UUID=$(id -u)
+UUID=$(( UUID + 0 ))
+I_OK="✔"
+I_KO="✖️"
+if [ $UUID != 0 ]; then
+	echo "${I_KO}\tInicia el Script como ROOT, para que funcione correctamente.\t${I_KO}";
+	exit 1;
+else
+  cd /root
+  echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" >> /etc/apt/sources.list.d/mongodb-org-3.4.list
+  wget -qO- https://www.mongodb.org/static/pgp/server-3.4.asc?_ga=2.184220325.209513483.1503609550-378600733.1503609550 | apt-key add -
+  apt-get update
+  apt-get install -y mongodb-server
+  apt-get upgrade -y
+fi
--- nodejs8-aptget.sh
+++ nodejs8-aptget.sh
@@ -1,5 +1,15 @@
-cd /root
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
-apt-get update
-apt-get install -y mongodb-server
-apt-get upgrade -y
+#!/bin/sh
+UUID=$(id -u)
+UUID=$(( UUID + 0 ))
+I_OK="✔"
+I_KO="✖️"
+if [ $UUID != 0 ]; then
+	echo "${I_KO}\tInicia el Script como ROOT, para que funcione correctamente.\t${I_KO}";
+	exit 1;
+else
+  cd /root
+  curl -sL https://deb.nodesource.com/setup_8.x | bash -
+  apt-get update
+  apt-get install -y nodejs
+  apt-get upgrade -y
+fi
